1d6a326e0d98c21c0ee79dfde8b82725b90bc4f9
seamoon-dev <seamoon.dev@gmail.com>
2023-11-12 15:05:11 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기화 기술 :
    
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/seamoon/lec/fcpjtboard/LecFcpjtBoardApplication.java
@@ -0,0 +1,13 @@
+package com.seamoon.lec.fcpjtboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecFcpjtBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LecFcpjtBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seamoon/lec/fcpjtboard/LecFcpjtBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seamoon.lec.fcpjtboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecFcpjtBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
